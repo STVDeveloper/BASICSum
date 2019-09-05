25325b7abc198d6026846af5ab7fbd50ff8a47e7
STV Developer <info@symtechven.com>
2019-09-05 17:06:33 -0400

    Changing Background color

--- FirstGit/Form1.Designer.vb
+++ FirstGit/Form1.Designer.vb
@@ -87,6 +87,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.InactiveCaption
         Me.ClientSize = New System.Drawing.Size(289, 120)
         Me.Controls.Add(Me.GroupBox1)
         Me.Name = "Form1"
